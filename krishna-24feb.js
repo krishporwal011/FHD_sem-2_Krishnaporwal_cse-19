b1a3014a80bc6cedd21cf0ffc1a5e91a97a9626e
Krishporwal011 <krishporwal486@gmail.com>
2026-02-24 12:23:07 +0530

    24feb

---
+++ krishna-24feb.js
@@ -0,0 +1,36 @@
+//console.log("introduction to Variables in JS");
+// var variable:
+/*var name = "Monika";
+var name = "Singh";
+name = "Monika Singh";
+console.log(name);*/
+// let variable:
+//let name = "Monika";
+//let name = "Singh";
+//name= "Monika Singh";
+//console.log(name);
+/*{
+    var a=10;
+    let b=20;
+    let sum = a+b;
+    console.log(sum);
+}
+
+console.log(a); // error: a is not defined*/
+//const variable:
+//var name;
+//console.log(name); // error: Assignment to constant variable.*/
+//Data types in JS:
+let name = "Alice";        // String
+let age = 30;              // Number
+let isStudent = true;      // Boolean
+let score;                 // Undefined
+let car = null;            // Null
+let person = { name: "Bob", age: 25 }; // Object
+console.log(typeof name); // string
+console.log(typeof age);  // number
+
+console.log(typeof isStudent);  // boolean
+console.log(typeof score);      // undefined
+console.log(typeof car);        // object
+console.log(typeof person);     // object
